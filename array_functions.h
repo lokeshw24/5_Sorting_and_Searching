c534751ef66ada9fe49962d02fa9b33ab775c973
Lokesh Walase <flokeshw24@gmail.com>
2013-02-22 11:13:52 +0530

    added a NOTE in array_functions.h for "arr_size" !! A GAMBLE indeed !!

--- array_functions.h
+++ array_functions.h
@@ -5,6 +5,9 @@
 }
 
 void print_array( int arr[] , int arr_size ){
+	/*NOTE : arr_size is the position of the last element in an array.
+	 * And not the total no. of elements in an array.
+	 */
 	int i=0 ;
 	arr_size++;
 
